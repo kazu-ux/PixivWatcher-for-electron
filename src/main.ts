29ce898dd6ac9238d375721064ed50f82315591b
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-04-15 08:16:06 +0900

    update: リファラをpixivに変える処理を追加

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
-import path from "node:path";
-import { BrowserWindow, app } from "electron";
+import path from 'node:path';
+import { BrowserWindow, app, session } from 'electron';
 
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      preload: path.resolve(__dirname, "preload.js"),
+      preload: path.resolve(__dirname, 'preload.js'),
     },
   });
 
-  mainWindow.loadFile("dist/index.html");
-  // mainWindow.webContents.openDevTools({ mode: "detach" });
+  mainWindow.loadFile('dist/index.html');
+  mainWindow.webContents.openDevTools({ mode: 'detach' });
+  mainWindow.webContents.session.webRequest.onBeforeSendHeaders(
+    {
+      urls: ['https://i.pximg.net/*'],
+      // types: ['xmlhttprequest'],
+    },
+    (detail, cb) => {
+      let { requestHeaders } = detail;
+      requestHeaders = Object.assign(requestHeaders, {
+        Referer: 'https://www.pixiv.net',
+      });
+      cb({ requestHeaders });
+    }
+  );
 });
 
-app.once("window-all-closed", () => app.quit());
+app.once('window-all-closed', () => app.quit());
