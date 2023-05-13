80521c3a1a79e90373bacd8140d2dc2ceb7cf13c
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-05-14 05:30:21 +0900

    update: 汎用性の向上

--- src/web/utils/create_search_url.ts
+++ src/web/utils/create_search_url.ts
@@ -1,9 +1,22 @@
 import { useAtom } from 'jotai';
 import { searchWordAtom } from '../atoms/atom';
+import { SearchQuery } from '../types/type';
 
-const createSearchURL = (searchWord: string) => {
-  const url = `https://www.pixiv.net/ajax/search/illustrations/${searchWord}?word=${searchWord}&order=date_d&mode=all&p=1&s_mode=s_tag&type=illust_and_ugoira&lang=ja`;
-  return url;
+const createSearchURL = (searchQuery: SearchQuery) => {
+  const url = new URL(
+    `${searchQuery['searchTarget'][0]}/${searchQuery.searchWord}`,
+    'https://www.pixiv.net/ajax/search/'
+  );
+  const params = url.searchParams;
+  params.set('word', searchQuery.searchWord);
+  params.set('order', 'date_d');
+  params.set('mode', searchQuery.targetAge);
+  params.set('p', '1');
+  params.set('s_mode', searchQuery.searchMethod);
+  params.set('type', searchQuery.searchTarget[1]);
+  params.set('lang', 'ja');
+
+  return url.href;
 };
 
 export default createSearchURL;
