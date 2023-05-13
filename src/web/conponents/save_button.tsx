baf50a2d8f2445533413af21a0fc6b2ccaaa122a
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-05-14 05:25:08 +0900

    update: 新着通知するタグの登録方法を変更

--- src/web/conponents/save_button.tsx
+++ src/web/conponents/save_button.tsx
@@ -5,25 +5,30 @@ import {
   worksAtom,
   searchWordAtom,
   watchWorksAtom,
+  searchUrlAtom,
 } from '../atoms/atom';
 
 import createSearchURL from '../utils/create_search_url';
+import { WatchWork } from '../types/type';
 
 const SaveButton = () => {
   const [searchWord] = useAtom(searchWordAtom);
   const [worksData] = useAtom(worksAtom);
   const [watchWorks, setWatchWorks] = useAtom(watchWorksAtom);
   const [darkMode, setDarkMode] = useAtom(darkModeAtom);
+  const [searchURL] = useAtom(searchUrlAtom);
 
   const handleButton = async () => {
-    /*     const hasDisplayName = await ChromeStorage.findWatchWork(searchWord);
-    if (hasDisplayName) return alert('すでに登録済みです'); */
-    /* setWatchWorks([
-      ...watchWorks,
-      { displayName: searchWord, worksData, url: createSearchURL(searchWord) },
-    ]);
-    setDarkMode(!darkMode);
-    console.log(darkMode); */
+    const now = new Date().getTime();
+    const watchWork: WatchWork = {
+      displayName: searchWord,
+      WorkData: worksData,
+      url: searchURL,
+      category: 'tag',
+    };
+    console.log(watchWorks);
+
+    setWatchWorks([...watchWorks, watchWork]);
   };
 
   return <Button onClick={handleButton}>新着通知に登録</Button>;
