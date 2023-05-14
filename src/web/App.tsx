af5b5a8ce03b05414f47858a807d4345f1984b1e
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-05-14 14:54:37 +0900

    update: homeコンポーネントに分離

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -3,15 +3,12 @@ import './App.css';
 import PermanentDrawerLeft from './containers/permanent_drawer';
 import Header from './conponents/header';
 
-import { Stack } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import About from './pages/about';
 import Settings from './pages/settings';
-import IllustList from './containers/illust_list';
-import SearchForm from './containers/search_form/search_form';
-import SaveButton from './conponents/save_button';
-import { DRAWERWIDTH } from './consts/const';
+
 import FeedPage from './pages/feed_page';
+import Home from './pages/home';
 
 export const App = () => {
   return (
@@ -19,25 +16,7 @@ export const App = () => {
       <PermanentDrawerLeft
         body={
           <Routes>
-            <Route
-              path='/'
-              element={
-                <Stack
-                  direction='column'
-                  justifyContent='center'
-                  alignItems='center'
-                  spacing={2}
-                  sx={{
-                    width: `calc(100% - ${DRAWERWIDTH}px)`,
-                    ml: `${DRAWERWIDTH}px`,
-                  }}
-                >
-                  <SearchForm></SearchForm>
-                  <SaveButton></SaveButton>
-                  <IllustList></IllustList>
-                </Stack>
-              }
-            />
+            <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/settings' element={<Settings />} />
             <Route path='/feed' element={<FeedPage />} />
