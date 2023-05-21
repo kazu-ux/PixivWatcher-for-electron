42b7979875b5774d7cab9e229c800d0c5f417594
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-05-22 05:21:13 +0900

    update: フィードページの最下部に余白を追加 スクロールのみで全ての作品を閲覧済みにできる

--- src/web/pages/feed_page.tsx
+++ src/web/pages/feed_page.tsx
@@ -1,5 +1,12 @@
 import WorkList from '../containers/worksList/work_list';
 
 export default function FeedPage() {
-  return <WorkList />;
+  return (
+    <div>
+      <WorkList />
+      <div style={{ minHeight: '95vh', minWidth: '50vh' }}>
+        <div style={{ paddingTop: '90vh' }}>全て閲覧しました！</div>
+      </div>
+    </div>
+  );
 }
