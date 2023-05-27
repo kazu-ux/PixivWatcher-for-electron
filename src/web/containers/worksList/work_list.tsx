4e726b14f5edecea887ff3016db2ec346d79209d
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-05-28 07:07:17 +0900

    clean: リファクタリング

--- src/web/containers/worksList/work_list.tsx
+++ src/web/containers/worksList/work_list.tsx
@@ -1,22 +1,20 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import './work_list.css';
 
-import { useAtom, useAtomValue } from 'jotai';
+import { useAtom } from 'jotai';
 import { updateViewedWorksAtom, worksAtom } from '../../atoms/atom';
 import WorkCard from './work_card';
 import classNames from 'classnames';
-import useInterval from '../../customHooks/useInterval';
+
 import { produce } from 'immer';
-import { WorkData } from '../../types/type';
+
 import getWatchWorkId from '../../utils/getWatchWorkId';
 import useMutationObserver from '../../customHooks/useMutationObserver';
 
 export default function WorkList() {
   const [workData] = useAtom(worksAtom);
-  const [viewedWorksCount, setViewedWorksCount] = useState(0);
   const [viewedWorks, updateViewedWork] = useAtom(updateViewedWorksAtom);
 
-  const viewedWorkIds = useRef<string[]>([]);
   const ref = useRef<HTMLDivElement>(null);
 
   const mutationObserverCallback = (records: MutationRecord[]) => {
@@ -52,44 +50,6 @@ export default function WorkList() {
       .forEach((element) => element.classList.add('hidden'));
   }, [workData]);
 
-  // const watchWorkId = useCallback(() => getWatchWorkId(), []);
-  // console.log(watchWorkId());
-
-  // console.log(watchWorkId);
-
-  // useInterval(() => {
-  //   const newViewedWorks = Array.from(document.querySelectorAll('.viewed')).map(
-  //     (element) => element.id
-  //   );
-
-  //   viewedWorkIds.current = newViewedWorks;
-  //   setViewedWorksCount(newViewedWorks.length);
-  // }, 100);
-
-  // useEffect(() => {
-  //   if (!watchWorkId || !viewedWorkIds.current.length) return;
-
-  //   const newViewedWorks = produce(viewedWorks, (draft) => {
-  //     const uniqueWatchWorkIds = Array.from(
-  //       new Set([...(draft[watchWorkId] ?? []), ...viewedWorkIds.current])
-  //     );
-  //     draft[watchWorkId] = uniqueWatchWorkIds;
-  //   });
-  //   setViewedWorks(newViewedWorks);
-  // }, [viewedWorksCount]);
-
-  // const callback = useCallback(
-  //   (element: HTMLDivElement | null, data: WorkData) => {
-  //     window.scrollTo(0, 0);
-
-  //     if (!watchWorkId) return;
-  //     /* if ((viewedWorks[watchWorkId] ?? ['']).includes(data.id)) {
-  //       element?.classList.add('hidden');
-  //     } */
-  //   },
-  //   [watchWorkId()]
-  // );
-
   return (
     <div
       ref={ref}
@@ -103,9 +63,6 @@ export default function WorkList() {
     >
       {workData.map((data, index) => (
         <div
-          // ref={(element) => {
-          //   callback(element, data);
-          // }}
           key={data.id}
           id={data.id}
           className={
